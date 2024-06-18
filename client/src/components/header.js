c736a9685753443888a439dc5dd2ce81bbc31b35
Maxim <golub94@mail.ru>
2024-06-19 01:13:37 +0300

    comm

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -8,7 +8,6 @@ import sign from '../assets/icons/sign.svg';
 import wifi from '../assets/icons/wifi.svg';
 import crown from '../assets/icons/crown.svg';
 import cube from '../assets/icons/cube.svg';
-import iconBg from '../assets/icons/iconBg.svg';
 
 const Header = () => {
     const menuItems = [
