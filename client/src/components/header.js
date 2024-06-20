dd96923b407041797ae942787b69893916557d79
Maxim <golub94@mail.ru>
2024-06-20 19:49:57 +0300

    Merge remote-tracking branch 'origin/master'

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import '../header.css';
 
 import caseIcon1 from '../assets/icons/case.svg';
@@ -15,17 +15,32 @@ const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false);
 
     const menuItems = [
-        { text: 'кейсы', href: '#', icon: caseIcon1 },
-        { text: 'апгрейд', href: '#', icon: upgrade },
-        { text: 'контракты', href: '#', icon: contract }
+        {text: 'кейсы', href: '#', icon: caseIcon1},
+        {text: 'апгрейд', href: '#', icon: upgrade},
+        {text: 'контракты', href: '#', icon: contract}
     ];
 
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
     };
 
-    const coloredSections = Array.from({ length: 10 }, (v, i) => (
-        <div className="colored-section" key={i}></div>
+    const imagePaths = [
+        'https://community.akamai.steamstatic.com/economy/image/6TMcQ7eX6E0EZl2byXi7vaVKyDk_zQLX05x6eLCFM9neAckxGDf7qU2e2gu64OnAeQ7835dX52LNfDY0jhyo8DEiv5dYOaw7rLczQfG2JulcKTk/360fx360f',
+        'https://steamcommunity-a.akamaihd.net/economy/image/6TMcQ7eX6E0EZl2byXi7vaVKyDk_zQLX05x6eLCFM9neAckxGDf7qU2e2gu64OnAeQ7835Je5WHNfDY0jhyo8DEiv5dYO607rLc2Rv2_pCV1NYc',
+        'https://community.akamai.steamstatic.com/economy/image/6TMcQ7eX6E0EZl2byXi7vaVKyDk_zQLX05x6eLCFM9neAckxGDf7qU2e2gu64OnAeQ7835Ff52LHfDY0jhyo8DEiv5dQOKo7rbw3Q_ndKTc8eQ/360fx360f',
+        'https://steamuserimages-a.akamaihd.net/ugc/1844802808246288843/666C70735165C0F9B434C0F7323A2C93714C03B4/',
+        'https://chicks-products.s3.amazonaws.com/2a841f71-89bc-4da7-8326-fefb8fb2ff0e',
+        'https://steamcommunity-a.akamaihd.net/economy/image/6TMcQ7eX6E0EZl2byXi7vaVKyDk_zQLX05x6eLCFM9neAckxGDf7qU2e2gu64OnAeQ7835Je5mPFfDY0jhyo8DEiv5dYPKA7rrY0Rv64OZ_IjGA',
+        'https://steamcommunity-a.akamaihd.net/economy/image/6TMcQ7eX6E0EZl2byXi7vaVKyDk_zQLX05x6eLCFM9neAckxGDf7qU2e2gu64OnAeQ7835dc5WLBfDY0jhyo8DEiv5daOK46rbczQfC_7rW9mjs',
+        'https://files.facepunch.com/rust/item/rifle.lr300_512.png',
+        'https://steamcommunity-a.akamaihd.net/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpovbSsLQJfwOfBfThW-NOJlZG0hOPxNrfunWVY7sBOguzA45W72wXn-ENtMjinIICTJwM6YV3W-Vi9k73mhZW16p3NzHRnsyNzsHrdmgv3309CdQ48_A',
+        'https://steamcommunity-a.akamaihd.net/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpou7uifDhzw8zAcCdD_tiJlpKKgfjLP7LWnn8fvZR30r2SpY3wiQDiqks6amqmIYaRJFQ3NAnZ_1Htlenoh5-_vMnPnWwj5HeouX7iBw',
+    ];
+
+    const coloredSections = imagePaths.map((imageSrc, i) => (
+        <div className="colored-section" key={i}>
+            <img src={imageSrc} alt={`Section ${i + 1}`} className="overlay-image"/>
+        </div>
     ));
 
     return (
