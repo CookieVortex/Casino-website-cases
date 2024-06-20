771ddefebe3c9204bd97599bcf8f4f7688ef68e2
Maxim <golub94@mail.ru>
2024-06-20 09:35:14 +0300

    Merge remote-tracking branch 'origin/master'

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -34,15 +34,17 @@ const Header = () => {
                 <div className="container">
                     <div className="header-inner">
                         <div className="small-logo-wrapper">
-                            <img src={smallLogo} alt="Маленький логотип" className="small-logo" />
+                            <img src={smallLogo} alt="Маленький логотип" className="small-logo"/>
                         </div>
 
                         <div className="logo-menu-wrapper">
                             <div className="logo-wrapper">
                                 <a href="/" className="logo-link">
                                     <div className="logo-large">
-                                        <img src="https://rustbox.io/assets/icons/logo.svg" alt="Логотип" className="large-logo" />
-                                        <img src={smallLogo} alt="Маленький логотип" className="small-logo-in-responsive" />
+                                        <img src="https://rustbox.io/assets/icons/logo.svg" alt="Логотип"
+                                             className="large-logo"/>
+                                        <img src={smallLogo} alt="Маленький логотип"
+                                             className="small-logo-in-responsive"/>
                                     </div>
                                 </a>
                             </div>
@@ -50,19 +52,19 @@ const Header = () => {
                                 {menuItems.map((item, index) => (
                                     <span key={index} className="menu-item">
                                         <a href={item.href} className="menu-link">
-                                            <img src={item.icon} alt={item.text} className="menu-icon" />
+                                            <img src={item.icon} alt={item.text} className="menu-icon"/>
                                             {item.text}
                                         </a>
                                     </span>
                                 ))}
                             </nav>
                             <button className="menu-toggle" onClick={toggleMenu}>
-                                <img src={menuIcon} alt="Menu" className="menu-icon-svg" />
+                                <img src={menuIcon} alt="Menu" className="menu-icon-svg"/>
                             </button>
                         </div>
                         <div className="login-button-wrapper">
                             <button className="login-button">
-                                <img src={sign} alt="Steam" className="login-button-icon" />
+                                <img src={sign} alt="Steam" className="login-button-icon"/>
                                 <span className="login-button-text">Войти</span>
                             </button>
                         </div>
@@ -73,29 +75,34 @@ const Header = () => {
             <div className="block-wrapper">
                 <div className="container">
                     <div className="blocks-container">
+
                         <div className="block first-section">
                             <div className="wifi-icon-wrapper">
-                                <img src={wifi} alt="Wifi" className="wifi-button-icon" />
+                                <img src={wifi} alt="Wifi" className="wifi-button-icon"/>
                             </div>
                             <span className="wifi-count">1</span>
                             <span className="wifi-online">online</span>
                         </div>
+
                         <div className="block vertical-sections">
                             <div className="second-section">
-                                <img src={crown} alt="Crown" className="crown-icon" />
+                                <img src={crown} alt="Crown" className="crown-icon"/>
                             </div>
                             <div className="third-section">
-                                <img src={cube} alt="Cube" className="cube-icon" />
+                                <img src={cube} alt="Cube" className="cube-icon"/>
                             </div>
                         </div>
+
                         <div className="block right-block">
                             <div className="colored-sections">
                                 {coloredSections}
                             </div>
                         </div>
+
                     </div>
                 </div>
             </div>
+
         </div>
     );
 };
