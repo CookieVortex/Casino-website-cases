c98d3ae1ae05beff9def33d84460417b4ed0279a
Maxim <golub94@mail.ru>
2024-06-30 18:09:48 +0300

    Merge remote-tracking branch 'origin/master'

--- client/src/components/EventsSection.js
+++ client/src/components/EventsSection.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './EventsSection.css';
+import smallIcon from '../assets/images/person.png';
 
 const EventsSection = () => {
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        setLoaded(true);
+    }, []);
+
     return (
         <div className="events-section">
             <div className="left-section">
+                <div className={`small-icon-container ${loaded ? 'loaded' : ''}`}>
+                    <img className="small-icon" src={smallIcon} alt=""/>
+                </div>
                 <button className="event-button">к ивенту</button>
             </div>
             <div className="right-section">
                 <div className="events-section-title">
-
                 </div>
             </div>
         </div>
