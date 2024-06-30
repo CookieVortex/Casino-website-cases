aea7d25c9d17d204a355f25039c9f67e292c99e6
Maxim <golub94@mail.ru>
2024-06-30 14:44:38 +0300

    Merge remote-tracking branch 'origin/master'

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/header';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import EventsSection from './components/EventsSection';
 
 const App = () => {
-
     return (
         <GoogleOAuthProvider clientId="616649422310-vbf57v2j34ql4qhkigom1itg1fv8i3uk.apps.googleusercontent.com">
             <Header />
+            <EventsSection />
         </GoogleOAuthProvider>
     );
 };
---
+++ client/src/components/EventsSection.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './EventsSection.css';
+
+const EventsSection = () => {
+    return (
+        <div className="events-section">
+            <div className="left-section">
+                <button className="event-button">к ивенту</button>
+            </div>
+            <div className="right-section">
+                <div className="events-section-title">
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default EventsSection;
