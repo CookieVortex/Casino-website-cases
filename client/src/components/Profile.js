a2be91821582a80b1f21ef5fc35e6e7880bce802
Maxim <golub94@mail.ru>
2024-07-06 00:28:28 +0300

    Merge remote-tracking branch 'origin/master'

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,17 +1,18 @@
-import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import React, {useState} from 'react';
+import {useLocation} from 'react-router-dom';
 import './Profile.css';
-import { ToastContainer, toast } from 'react-toastify';
+import {ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Paper from "../assets/icons/paper.svg";
 import Wallet from "../assets/icons/wallet.svg";
 import setprofile from "../assets/icons/setprofile.svg";
 import logoutprofile from "../assets/icons/logoutprofile.svg";
-import { Tooltip } from 'react-tooltip';
+import {Tooltip} from 'react-tooltip';
+import con2 from '../assets/images/con2.webp';
 
 const Profile = () => {
     const location = useLocation();
-    const { profile } = location.state || {};
+    const {profile} = location.state || {};
     const [copySuccess, setCopySuccess] = useState(false);
     const [inputValue, setInputValue] = useState("");
 
@@ -22,7 +23,7 @@ const Profile = () => {
     const copyTextToClipboard = async (text) => {
         try {
             await navigator.clipboard.writeText(text);
-            toast.success('Текст успешно скопирован в буфер обмена!', { autoClose: 3000 });
+            toast.success('Текст успешно скопирован в буфер обмена!', {autoClose: 3000});
             setCopySuccess(true);
             setTimeout(() => setCopySuccess(false), 5000);
         } catch (err) {
@@ -49,7 +50,7 @@ const Profile = () => {
             <div className="section1">
                 <div className="profile-top">
                     <div className="profile-left">
-                        <img src={profile.picture} className="avatar" alt={profile.name} />
+                        <img src={profile.picture} className="avatar" alt={profile.name}/>
                         <div className="profile-details">
                             <span className="profile-name">{firstName}</span>
                         </div>
@@ -59,14 +60,14 @@ const Profile = () => {
                         <a className="my-anchor-element">
                             <button onClick={() => copyTextToClipboard(profile.googleId)} className="button1">
                                 <span className="info-button"><img src={Paper} alt="Paper"
-                                                                   className="paper-icon" />ID</span>
+                                                                   className="paper-icon"/>ID</span>
                                 <span className="info-id">{profile.googleId}</span>
                             </button>
                         </a>
-                        <div className="button2"><img src={Wallet} alt="Wallet" className="wallet-icon" /></div>
-                        <div className="button3"><img src={setprofile} alt="SettingsProfile" className="setprofile" />
+                        <div className="button2"><img src={Wallet} alt="Wallet" className="wallet-icon"/></div>
+                        <div className="button3"><img src={setprofile} alt="SettingsProfile" className="setprofile"/>
                         </div>
-                        <div className="button4"><img src={logoutprofile} alt="Quit" className="logoutprofile" /></div>
+                        <div className="button4"><img src={logoutprofile} alt="Quit" className="logoutprofile"/></div>
                     </div>
                 </div>
                 <div className="form-container">
@@ -81,9 +82,21 @@ const Profile = () => {
                 </div>
             </div>
             <div className="section">
-                Раздел 2
+                <div className="case-container">
+                    <div className="case-title">Любимый Кейс</div>
+                    <div className="case-title-bonus">Бонусный кейс</div>
+                    <button className="open-button">Открыть</button>
+                </div>
+                <img src={con2} alt="Case" className="case-image"/>
             </div>
-            <div className="section">Раздел 3</div>
+            <div className="section">
+                <div className="case-container">
+                    <div className="case-title">Лучший дроп</div>
+                    <div className="case-title-bonus">Facepunch TShirt</div>
+                    <button className="open-button">3,98€</button>
+                </div>
+                <img src={con2} alt="Case" className="case-image"/>
+            </div>
             <Tooltip anchorSelect=".my-anchor-element" place="top">
                 Скопировать
             </Tooltip>
