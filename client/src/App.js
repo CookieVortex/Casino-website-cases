4f581e3e8c6a396cbf3a1dae3c9554b1aff5c1ee
Maxim <golub94@mail.ru>
2024-07-08 01:51:53 +0300

    Merge remote-tracking branch 'origin/master'

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,27 @@
+// App.js
+
 import React from 'react';
 import './App.css';
 import Header from './components/header';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {GoogleOAuthProvider} from '@react-oauth/google';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Profile from './components/Profile';
 import Main from './components/Main';
+import EventsSection from './components/EventsSection';
 
 const App = () => {
     return (
         <GoogleOAuthProvider clientId="616649422310-vbf57v2j34ql4qhkigom1itg1fv8i3uk.apps.googleusercontent.com">
             <BrowserRouter>
-                <Header />
+                <Header/>
                 <Routes>
-                    <Route path="/" element={<Main />} />
-                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/" element={
+                        <>
+                            <EventsSection/>
+                            <Main/>
+                        </>
+                    }/>
+                    <Route path="/profile" element={<Profile/>}/>
                 </Routes>
             </BrowserRouter>
         </GoogleOAuthProvider>
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,12 +1,35 @@
+// Main.js
+
 import React from 'react';
-import EventsSection from './EventsSection';
 import './Main.css';
+import loot1 from '../assets/images/loot1.png';
+import loot2 from '../assets/images/loot2.png';
+import loot3 from '../assets/images/loot3.png';
+import loot4 from '../assets/images/loot4.png';
+import loot5 from '../assets/images/loot5.png';
+import loot6 from '../assets/images/loot6.png';
 
 const Main = () => {
-    return (
-        <div className="main">
-            <EventsSection />
+    const sections = [
+        { img: loot1, label: '1.80 €' },
+        { img: loot2, label: '2.65 €' },
+        { img: loot3, label: '3.50 €' },
+        { img: loot4, label: '4.75 €' },
+        { img: loot5, label: '7.20 €' },
+        { img: loot6, label: '9.10 €' },
+    ];
 
+    return (
+        <div className="main-section">
+            {sections.map((section, index) => (
+                <div key={index} className="sub-section">
+                    <img src={section.img} alt={`Loot ${index + 1}`} className={`loot${index + 1}`} />
+                    <div className="button-container">
+                        <button className="loot-button">{section.label}</button>
+                    </div>
+                    <div className="new-label">NEW</div>
+                </div>
+            ))}
         </div>
     );
 };
